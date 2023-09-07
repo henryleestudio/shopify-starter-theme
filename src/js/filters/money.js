9dbd4cd497400c86cbb9640f845c5eac366ae77e
Henry Lee - Dev <henrylee.contact@gmail.com>
2023-09-07 15:38:12 -0400

    adding new starter files with laravel-mix

--- src/js/filters/money.js
+++
@@ -1,6 +0,0 @@
-
-
-Vue.filter('money', function (value, sign = '$') {
-  if (!value) return 0;
-  return sign + (value/100).toFixed( 2 );
-})
--- src/js/header.js
+++
@@ -1,6 +0,0 @@
-
-
-// PREVENT INSIDE CLICK DROPDOWN 
-$('body').on("click", ".dropdown-menu", function (e) {
-    $(this).parent().is(".show") && e.stopPropagation();
-});
